a8957fe6b44841712bd260261793e00d7d32f676
alon galperin <alonga90@gmail.com>
2020-08-28 17:30:53 +0300

    Backup project for moving to ubuntu

--- ticketing/client/pages/index.js
+++ ticketing/client/pages/index.js
@@ -1,3 +1,27 @@
-export default () => {
-  return <h1>Landing Page</h1>;
+import axios from "axios";
+
+const LandingPage = ({ currentUser }) => {
+  console.log(currentUser);
+
+  return <h1>Landing Page..as.</h1>;
 };
+
+LandingPage.getInitialProps = async () => {
+  if (typeof window === "undefined") {
+    // we are on the server
+    // requests should be made to http://ingress-nginx.ingress-nginx...
+    const { data } = await axios.get(
+      "http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser"
+    );
+
+    return { data };
+  } else {
+    // we are on the browser
+    // requests should be made to "" empty string
+    const { data } = await axios.get("/api/users/currentuser");
+    return data;
+  }
+  return {};
+};
+
+export default LandingPage;
